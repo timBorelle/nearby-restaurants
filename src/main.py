80a5446a197552088ef414afe8c4639cf889641e
timBorelle <tim-borelle@protonmail.com>
2023-02-06 00:13:49 +0100

    show result

--- src/main.py
+++ src/main.py
@@ -1,43 +1,70 @@
 # coding: utf-8
 
 import sys
-#import pandas as pd
 import json
 import haversine as hs
 from haversine import Unit
 from datetime import datetime
+import logging
 
-def main(argv):
-    input_latitude = float(argv[0].split("=", 1)[1])
-    input_longitude = float(argv[1].split("=", 1)[1])
-    input_radius = float(argv[2].split("=", 1)[1])
+logging.basicConfig()
+logger = logging.getLogger("execution-time")
+logger.setLevel(logging.INFO)
+
+argv = sys.argv[1:]
+input_latitude = 0.0
+input_longitude = 0.0
+input_radius = 0.0
+
+def main():
     geojson_file = "./data/restaurants_paris.geojson"
-    read_geojson(input_latitude, input_longitude, input_radius, geojson_file)
+    data = load_data(geojson_file)
+    nearby_stations = query_search(data)
+    show_output(nearby_stations)
 
-def read_geojson(input_latitude, input_longitude, input_radius, geojson_file):
-    print("read .geojson file ...")
-    input_loc = (input_latitude, input_longitude)
-    nearby_stations = []
+# load geojson data file 
+def load_data(geojson_file):
     start_loading = datetime.now()
     with open(geojson_file) as data_file:    
         data = json.load(data_file)
         end_loading = datetime.now()
-    print(f"Loading execution time : {(end_loading - start_loading).total_seconds() * 1000} ms.")
-    point_data = data["features"][0]["geometry"]
-    geometry_type = point_data["type"]
-    if geometry_type == "Point":
-        latitude = point_data["coordinates"][1]
-        longitude = point_data["coordinates"][0]
-        current_loc = (latitude, longitude)
-        res_radius = calculate_distance_between_two_points(input_loc, current_loc)
-        print("res_radius : ", res_radius)
-        if res_radius <= input_radius:
-            name = data["features"][0]["properties"]["name"]
-            station_data = "name:" + name + ",longitude:" + str(longitude) + ",latitude:" + str(latitude) + ",distance:" + str(res_radius)
-            nearby_stations.append(station_data)
+    logger.debug("{} ran in {} ms.".format(load_data.__name__, (end_loading - start_loading).total_seconds() * 1000))
+    return data
 
+# calculate distance between two point
 def calculate_distance_between_two_points(loc1: tuple, loc2: tuple):
     return round(hs.haversine(loc1, loc2, unit=Unit.METERS), 2)
 
+def query_search(data):
+    start_query_search = datetime.now()
+    input_latitude = float(argv[0].split("=", 1)[1])
+    input_longitude = float(argv[1].split("=", 1)[1])
+    input_radius = float(argv[2].split("=", 1)[1])
+    input_loc = (input_latitude, input_longitude)
+    nearby_stations = []
+    # iterate over each restaurant
+    for r in data["features"]:
+        point_data = r["geometry"]
+        geometry_type = point_data["type"]
+        if geometry_type == "Point":
+            latitude = point_data["coordinates"][1]
+            longitude = point_data["coordinates"][0]
+            current_loc = (latitude, longitude)
+            res_radius = calculate_distance_between_two_points(input_loc, current_loc)
+            if res_radius <= input_radius:
+                name = r["properties"]["name"]
+                station_data = "name:" + name + ",longitude:" + str(longitude) + ",latitude:" + str(latitude) + ",distance:" + str(res_radius)
+                # add station data to nearby_stations list
+                nearby_stations.append(station_data)
+    end_query_search = datetime.now()
+    logger.debug("{} ran in {} ms.".format(query_search.__name__, (end_query_search - start_query_search).total_seconds() * 1000))
+    return nearby_stations
+
+    
+def show_output(nearby_stations: list):
+    if len(nearby_stations) != 0:
+        for elem in nearby_stations:
+            print(elem)
+
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
