e897718503a86c229742c2da1f89a5a3d433a402
timBorelle <tim-borelle@protonmail.com>
2023-02-05 21:57:35 +0100

    calculate distance between two points

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@
 import sys
 #import pandas as pd
 import json
+import haversine as hs
+from haversine import Unit
 from datetime import datetime
 
 def main(argv):
@@ -27,6 +29,15 @@ def read_geojson(input_latitude, input_longitude, input_radius, geojson_file):
         latitude = point_data["coordinates"][1]
         longitude = point_data["coordinates"][0]
         current_loc = (latitude, longitude)
+        res_radius = calculate_distance_between_two_points(input_loc, current_loc)
+        print("res_radius : ", res_radius)
+        if res_radius <= input_radius:
+            name = data["features"][0]["properties"]["name"]
+            station_data = "name:" + name + ",longitude:" + str(longitude) + ",latitude:" + str(latitude) + ",distance:" + str(res_radius)
+            nearby_stations.append(station_data)
+
+def calculate_distance_between_two_points(loc1: tuple, loc2: tuple):
+    return round(hs.haversine(loc1, loc2, unit=Unit.METERS), 2)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
